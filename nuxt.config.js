537c14b8f2aeebfb2cae7d8f2a89da015cc6d776
AdamchukVladimir <vldmr.adamchuk@gmail.com>
2023-10-06 21:06:52 +0300

    add ssr:true

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   target: 'static',
+  ssr: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ['@nuxtjs/fontawesome'],
